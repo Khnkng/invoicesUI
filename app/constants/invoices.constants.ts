044e72c3d666caf10be63af3e2bda6ce5fe19371
ramaraju <rama.raju@bighalf.io>
2017-07-10 11:40:19 +0530

    invoices changes

--- app/constants/invoices.constants.ts
+++ app/constants/invoices.constants.ts
@@ -1,5 +1,5 @@
 export const INVOICE_PATHS={
-    INVOICE_PREFERENCE: "/Invoices/user/:id/company/:companyId/invoice/preference",
+    INVOICE_PREFERENCE: "/Invoices/users/:id/companies/:companyId/invoice/preference",
     INVOICE:"/Invoices/users/:id/companies/:companyId/invoice",
     INVOICE_PAY:"/Invoices/invoices/:invoiceID",
     INVOICE_DELETE:'/Invoices/users/:id/companies/:companyId/invoice/delete',
