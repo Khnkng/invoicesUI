d2db165da27c50c90857521dd5669526f4a6afa5
venkatqount <venki.kollikonda@qount.io>
2017-07-10 12:32:32 +0530

    changes to payment preview

--- app/components/InvoicePaymentPreview.component.ts
+++ app/components/InvoicePaymentPreview.component.ts
@@ -1,5 +1,5 @@
 
-import {Component} from "@angular/core";
+import {Component,Input} from "@angular/core";
 import {SwitchBoard} from "qCommon/app/services/SwitchBoard";
 import {ToastService} from "qCommon/app/services/Toast.service";
 import {LoadingService} from "qCommon/app/services/LoadingService";
@@ -21,38 +21,24 @@ declare let moment:any;
 })
 
 export class InvoicePaymentPreview{
-    localeFortmat:string='en-US';
-    routeSub:any;
     invoiceID:string;
-    invoiceData:any;
     customers: Array<any> = [];
     taskInvoices: Array<any> = [];
     itemInvoices: Array<any> = [];
-    hasInvoiceData: boolean = false;
+    invoiceData:any;
+    @Input()
+    set invoices(invoices:any){
+        this.invoiceData = invoices;
+        this.loadInvoiceData();
+    }
 
     constructor(private switchBoard: SwitchBoard, private _router:Router, private _route: ActivatedRoute, private toastService: ToastService,
                 private loadingService:LoadingService, private titleService:pageTitleService, private invoiceService: InvoicesService,private customerService: CustomersService){
-        this.titleService.setPageTitle("Add Payment To Invoice");
-        this.routeSub = this._route.params.subscribe(params => {
-            //this.invoiceID=params['invoiceID'];
-            this.invoiceID='88f194d9-6712-48d7-afc3-f2d6699fe8f9';
-            this.loadInvoiceData();
-            this.loadCustomers(Session.getCurrentCompany());
-        });
     }
 
     loadInvoiceData() {
-        let base = this;
-        this.loadingService.triggerLoadingEvent(true);
-        this.invoiceService.getInvoice(this.invoiceID).subscribe(invoices=>{
-            if(invoices) {
-                base.hasInvoiceData = true;
-                base.invoiceData = invoices;
-                base.taskInvoices =  _.filter(invoices.invoiceLines, function(invoice) { return invoice.type == 'task'; });
-                base.itemInvoices =  _.filter(invoices.invoiceLines, function(invoice) { return invoice.type == 'item'; });
-            }
-            this.loadingService.triggerLoadingEvent(false);
-        },error=>this.handleError(error));
+        this.taskInvoices =  _.filter(this.invoiceData.invoiceLines, function(invoice) { return invoice.type == 'task'; });
+        this.itemInvoices =  _.filter(this.invoiceData.invoiceLines, function(invoice) { return invoice.type == 'item'; });
     }
 
     loadCustomers(companyId: any) {
@@ -64,6 +50,8 @@ export class InvoicePaymentPreview{
                 this.loadingService.triggerLoadingEvent(false);
             });
     }
+
+
 
     handleError(error) {
         this.loadingService.triggerLoadingEvent(false);
