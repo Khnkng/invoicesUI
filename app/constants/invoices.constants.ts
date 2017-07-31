76ce775a3af0826d765823bb809cdd87c4b39456
Chandu <chandu.v@qount.io>
2017-07-31 17:12:41 +0530

    Invoice dashboard

--- app/constants/invoices.constants.ts
+++ app/constants/invoices.constants.ts
@@ -6,5 +6,6 @@ export const INVOICE_PATHS={
     INVOICE_PAYMENTS:"/Invoices/users/:id/companies/:companyId/invoice/payment",
     INVOICE_DELETE:'/Invoices/users/:id/companies/:companyId/invoice/delete',
     INVOICE_SENT:'/Invoices/users/:id/companies/:companyId/invoice/sent',
-    INVOICE_PAID:'/Invoices/users/:id/companies/:companyId/invoice/:invoiceId/state'
+    INVOICE_PAID:'/Invoices/users/:id/companies/:companyId/invoice/:invoiceId/state',
+    INVOICE_DASHBOARD_BOX: '/Invoices/users/:id/companies/:companyId/invoice/metrics'
 };
