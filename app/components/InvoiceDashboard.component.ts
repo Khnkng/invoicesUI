07a7f6951d641ca5adaba92beb163c5fff43cff4
nagaraju91 <nagaraju.thota@qount.io>
2017-11-06 11:46:30 +0530

    colors change fix

--- app/components/InvoiceDashboard.component.ts
+++ app/components/InvoiceDashboard.component.ts
@@ -598,6 +598,7 @@ export class InvoiceDashboardComponent {
                     });
                 });
                 this.customerAgingSummary={
+                    colors: this.chartColors,
                     chart: {
                         type: 'column',
                         style: {
